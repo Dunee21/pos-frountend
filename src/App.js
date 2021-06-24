82f4c2eab69bbfe9bb7c54f1407d50343d6e89f8
DUNEE YUSSIF <duneeyussif@gmail.com>
2021-06-24 00:18:04 -0700

    pos frount end

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./component/Header";
+import ItemsList from "./screens/ItemsList";
+function App() {
+  return (
+    <div>
+      <Header />
+      <ItemsList />
+    </div>
+  );
+}
+
+export default App;
